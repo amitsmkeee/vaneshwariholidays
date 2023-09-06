471fe87c9d9c2adcf8f53d81ce5534b81a76ebd6
amit kumar <amitkumar@amits-MacBook-Pro.local>
2023-09-06 21:13:44 +0530

    test3

--- web/contact-form-handler.php
+++ web/contact-form-handler.php
@@ -20,15 +20,28 @@
     if( empty($errors))
     {
         $to = $myemail;
-        $email_subject = "Contact form submission: $name";
+        $email_subject = "Contact form submission:". $name;
         $email_body = "You have received a new message. ".
-        " Here are the details:\n Name: $name \n ".
-        "Email: $email_address\n Message \n $message";
-        $headers = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-        $headers .= 'From: '.$to  . "\r\n";
-        echo mail($email_address, $email_subject, $email_body, $headers);
-        die;
+        " Here are the details:\n Name:". $name ."\n ".
+        "Email:". $email_address."\n Message \n". $message;
+        // $headers = 'MIME-Version: 1.0' . "\r\n";
+        // $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        // $headers .= 'From: '.$to  . "\r\n";
+        $headers = array(
+            'From' => '' . $to ,
+            'Reply-To' => ''. $to,
+            'X-Mailer' => 'PHP/' . phpversion()
+        );
+        // print_r($headers);
+        // echo "<-->";
+        // echo $email_address;
+        // echo "<-->";
+        // echo $email_body;
+        // echo "<-->";
+        // echo $email_subject;
+        // echo "<-->";
+        // die;
+        mail($email_address, $email_subject, $email_body, $headers);
         //redirect to the 'thank you' page
         header('Location: index.html');
     }
