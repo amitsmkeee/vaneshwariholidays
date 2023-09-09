8156df9d0775dcbd339d20e008cf122df3e44513
amit kumar <amitkumar@amits-MacBook-Pro.local>
2023-09-09 11:10:33 +0530

    phpmailer added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vaneshwariholidays/web',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '064a4f8312a62a6a63ab8ab57cde2c734c2294f6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.1',
+            'version' => '6.8.1.0',
+            'reference' => 'e88da8d679acc3824ff231fdc553565b802ac016',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'vaneshwariholidays/web' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '064a4f8312a62a6a63ab8ab57cde2c734c2294f6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
--- web/contact-form-handler.php
+++ web/contact-form-handler.php
@@ -1,8 +1,11 @@
 <?php
+   use PHPMailer\PHPMailer\PHPMailer;
+   require '../vendor/autoload.php';
+
     $errors = '';
     $myemail = 'info@vaneshwariholidays.com';//<-----Put Your email address here.
     if(empty($_POST['name'])  ||
-       empty($_POST['email']) || 
+       empty($_POST['email']) ||
        empty($_POST['message']))
     {
         $errors .= "\n Error: all fields are required";
@@ -10,39 +13,35 @@
     $name = $_POST['name'];
     $email_address = $_POST['email'];
     $message = $_POST['message'];
+
     if (!preg_match(
     "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
     $email_address))
     {
         $errors .= "\n Error: Invalid email address";
     }
-    
+
     if( empty($errors))
     {
-        $to = $myemail;
-        $email_subject = "Contact form submission:". $name;
-        $email_body = "You have received a new message. ".
-        " Here are the details:\n Name:". $name ."\n ".
-        "Email:". $email_address."\n Message \n". $message;
-        // $headers = 'MIME-Version: 1.0' . "\r\n";
-        // $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-        // $headers .= 'From: '.$to  . "\r\n";
-        $headers = array(
-            'From' => '' . $to ,
-            'Reply-To' => ''. $to,
-            'X-Mailer' => 'PHP/' . phpversion()
-        );
-        // print_r($headers);
-        // echo "<-->";
-        // echo $email_address;
-        // echo "<-->";
-        // echo $email_body;
-        // echo "<-->";
-        // echo $email_subject;
-        // echo "<-->";
-        // die;
-        mail($to, $email_subject, $email_body, $headers);
-        //redirect to the 'thank you' page
-        header('Location: index.html');
+        $mail = new PHPMailer;
+        $mail->isSMTP();
+        $mail->SMTPDebug = 0;
+        $mail->Host = 'smtp.hostinger.com';
+        $mail->Port = 587;
+        $mail->SMTPAuth = true;
+        $mail->Username = 'noreply@vaneshwariholidays.com';
+        $mail->Password = 'nOReply@987612345';
+        $mail->setFrom('noreply@vaneshwariholidays.com', 'Contact Us Form VaneshwariHolidays');
+        $mail->addReplyTo($email_address, $name);
+        $mail->addAddress('info@vaneshwariholidays.com', 'VaneshwariHolidays');
+        $mail->Subject = 'Received a contact information from '.$name;
+        $mail->Body = "Name: $name\nEmail: $email_address\nBody:$message";
+        if (!$mail->send()) {
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        } else {
+            echo 'The email message was sent.';
+        }
+    } else {
+            echo $errors;
     }
 ?>
