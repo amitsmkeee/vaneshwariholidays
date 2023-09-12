ee32ed5e10be3063b0b056806fce175f96776cfe
amit kumar <amitkumar@amits-MacBook-Pro.local>
2023-09-12 19:07:09 +0530

    db changes added

--- config/db.php
+++ config/db.php
@@ -2,13 +2,13 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=vaneshwari',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=u689144669_vaneshholidays',
+    'username' => 'u689144669_vaneshholidays',
+    'password' => '=ywgFJ5h',
     'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    //Schema cache options (for production environment)
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 3600,
+    'schemaCache' => 'cache',
 ];
